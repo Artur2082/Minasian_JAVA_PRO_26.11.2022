41607521ddce0fc846b5287ae71a12ce628608ea
Artur Minasian <Arthrmin@gmail.com>
2022-11-27 16:53:43 +0200

    adding in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+            Scanner scanner = new Scanner(System.in);
+            int number;
+            int mult = 1;
+            do {
+                System.out.println("Enter number from 2 to 8 ");
+                number = scanner.nextInt();
+            } while ( number < 2 || number > 8);
+            for (int i = 1; i <= number; i++) {
+                mult = mult * i;
+                System.out.println(i);
+            }
+            System.out.println(" Factorial " + number + "! = " + mult);
+        }
     }
-}
+
