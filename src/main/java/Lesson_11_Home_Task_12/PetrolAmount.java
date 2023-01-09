2e2092f97aeab702f2b398612411242d51fa955f
Artur Minasian <Arthrmin@gmail.com>
2023-01-09 21:14:07 +0200

    Add home task_12

---
+++ src/main/java/Lesson_11_Home_Task_12/PetrolAmount.java
@@ -0,0 +1,17 @@
+package Lesson_11_Home_Task_12;
+
+public class PetrolAmount {
+    private int amount;
+
+    public PetrolAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
