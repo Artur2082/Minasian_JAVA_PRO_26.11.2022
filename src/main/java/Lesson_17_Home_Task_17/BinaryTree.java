d283142f8f59e4e0630d8aabb2e57e6879a28c97
Artur Minasian <Arthrmin@gmail.com>
2023-01-29 16:50:30 +0200

    Add home task_17

---
+++ src/main/java/Lesson_17_Home_Task_17/BinaryTree.java
@@ -0,0 +1,29 @@
+package Lesson_17_Home_Task_17;
+
+public class BinaryTree {
+    public void add(Node node, int value) {
+        if (value < node.value) {
+            if (node.left != null) {
+                add(node.left, value);
+            } else {
+                System.out.println(" Add " + value + " to left of " + node.value);
+                node.left = new Node(value);
+            }
+        } else if (value > node.value) {
+            if (node.right != null) {
+                add(node.right, value);
+            } else {
+                System.out.println(" Add " + value + " to right of " + node.value);
+                node.right = new Node(value);
+            }
+        }
+    }
+
+    public void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.value);
+            traverseInOrder(node.right);
+        }
+    }
+}
---
+++ src/main/java/Lesson_17_Home_Task_17/Node.java
@@ -0,0 +1,13 @@
+package Lesson_17_Home_Task_17;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+        left = null;
+        right = null;
+    }
+}
