d3753b205d12938d573e11d0459223444c59f531
Artur Minasian <Arthrmin@gmail.com>
2023-03-24 10:58:38 +0200

    Add Tests

---
+++ src/main/java/U_Working_With_Tests/SimpleMathLibrary.java
@@ -0,0 +1,11 @@
+package U_Working_With_Tests;
+
+public class SimpleMathLibrary {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double minus(double a, double b) {
+        return a - b;
+    }
+}
